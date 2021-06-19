5b1120f3e1857eab63d03875bc35044376d598e4
Alex Pesch <apesch@google.com>
2021-06-19 16:01:14 -0400

    Docstrings.

--- utilities/converter_util.py
+++ utilities/converter_util.py
@@ -1,96 +1,69 @@
 import os
-import time
 import subprocess
-import datetime
 import logging
 
 
-def GetNewFiles(vid_list):
-  """ Return a list of files that are marked as 'NEW'.
-  """
-    # CSV schema -
-    # video_path, tcode_status, format, added, completed
-
-    to_process = []
-
-    for vid in vid_list[0]:
-        if vid[1] == 'NEW':
-            to_process.append(vid)
-
-    return to_process
-
-
 def Transcode(vid_file):
-    ffmpeg = '/usr/bin/ffmpeg'
+  """Spawn ffmpeg processes for transcoding from H264 -> H265
 
-    if not os.path.isfile(ffmpeg):
-        raise Exception('FFMPEG not found. Install it!')
+  Args:
+    vid_file: A string representing the file path of a video file.
 
-    #ffmpeg -i INPUT -c:v libx265 -c:a copy -x265-params crf=25 OUT.mov
-    #ffmpeg -i h264Input.mp4 -c:v libx265 -crf 16 -c:a copy h265output.mp4
-    orig_ext = vid_file[-4:]
-    ffmpeg_command = [
-            ffmpeg,
-            '-i',
-            '%s' % vid_file,
-            '-c:v',
-            'libx265',
-            '-c:a',
-            'copy',
-            '-y',
-            '-threads',
-            '16',
-            '-preset',
-            'veryfast',
-            vid_file.replace(orig_ext, '_new%s' % orig_ext)
-            ]
-    tcode = subprocess.Popen(ffmpeg_command)
+  Returns:
+    A subprocess object
 
-    logging.info('      Executing command: %s' % ' '.join(ffmpeg_command))
-    #while tcode.poll() is None:
-    #   time.sleep(0.5)
+  Raises:
+    Exception: If the FFMpeg binary is not found in /usr/bin/ffmpeg.
+  """
+  ffmpeg = '/usr/bin/ffmpeg'
 
-    #return_code = tcode.returncode
+  if not os.path.isfile(ffmpeg):
+      raise Exception('FFMPEG not found. Install it!')
 
-    return tcode
+  #ffmpeg -i INPUT -c:v libx265 -c:a copy -x265-params crf=25 OUT.mov
+  #ffmpeg -i h264Input.mp4 -c:v libx265 -crf 16 -c:a copy h265output.mp4
+  orig_ext = vid_file[-4:]
+  ffmpeg_command = [
+          ffmpeg,
+          '-i',
+          '%s' % vid_file,
+          '-c:v',
+          'libx265',
+          '-c:a',
+          'copy',
+          '-y',
+          '-threads',
+          '16',
+          '-preset',
+          'veryfast',
+          vid_file.replace(orig_ext, '_new%s' % orig_ext)
+          ]
+  tcode = subprocess.Popen(ffmpeg_command)
+
+  logging.info('      Executing command: %s' % ' '.join(ffmpeg_command))
+
+  return tcode
 
 
 def CheckFormat(vid_file):
-    media_info = '/usr/bin/mediainfo'
-    media_stats = subprocess.check_output(
-            [
-                media_info,
-                vid_file
-            ]).decode("utf-8") 
-    stats_list = media_stats.split('\n')
+  """Finds all the video files using mediainfo.
 
-    for stat in stats_list:
-        if stat.lower().startswith('format/info'):
-            print('Checking format of file: %s' % vid_file)
-            vid_format = stat.split(':')[1].strip(' ')
-            return vid_format
+  Args:
+    vid_file: A string representing the file path of a video file.
 
+  Returns:
+    vid_format: A string representing the video file format for a video file.
+  """
+  media_info = '/usr/bin/mediainfo'
+  media_stats = subprocess.check_output(
+          [
+              media_info,
+              vid_file
+          ]).decode("utf-8") 
+  stats_list = media_stats.split('\n')
 
-def HandleNewFiles(new_files):
-    # video_path, tcode_status, format, added, completed
-
-    completed_transcodes = []
-    failed_transcodes = []
-    attempt_count = 1
-    
-    new_h264 = [
-      vid for vid in new_files if CheckFormat(vid) == 'Advanced Video Codec'
-      ]
-    for vid_file in new_h264:
-        transcode_result = Transcode(vid_file)
-        if transcode_result == 0:
-            today = datetime.datetime.now().strftime('%Y-%b-%d')
-            completed_transcodes.append(
-              [
-                vid_file,
-                'SUCCESS',
-                'H265'
-                ]
-                )
-        else:
-            failed_transcodes.append(vid_file)
+  for stat in stats_list:
+      if stat.lower().startswith('format/info'):
+          print('Checking format of file: %s' % vid_file)
+          vid_format = stat.split(':')[1].strip(' ')
+          return vid_format
