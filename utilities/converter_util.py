136c485826d40b415687928a57b6cada88c20495
apesch85 <gorrilla86@gmail.com>
2021-06-19 14:38:10 -0400

    Update converter_util.py

--- utilities/converter_util.py
+++ utilities/converter_util.py
@@ -42,7 +42,7 @@ def Transcode(vid_file):
             'veryfast',
             vid_file.replace(orig_ext, '_new%s' % orig_ext)
             ]
-    tcode = subprocess.Popen(ffmpeg_command, stderr=subprocess.PIPE, stdout=subprocess.PIPE )
+    tcode = subprocess.Popen(ffmpeg_command, stdin=None, stdout=None, stderr=None )
     tcode.communicate()
 
     logging.info('      Executing command: %s' % ' '.join(ffmpeg_command))
